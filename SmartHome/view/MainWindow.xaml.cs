383924f0c288a13c9a2057084d95c72531f0f2c4
Arnaud Flaesch <arnaudflaesch@hotmail.com>
2016-07-06 14:09:37 +0200

    Ebauche de la création de la liste des pièces avec indicateurs liés aux capteurs
    Le code de MainWindow.xaml.cs permet de générer la liste des pièces avec une icône visant à indiquer le nombre de personne dans une pièce.
    Le code va être remplacé par des Binding dans le Xaml pour simplifier et gagner en lisibilité.

--- SmartHome/view/MainWindow.xaml.cs
+++ SmartHome/view/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,17 +20,39 @@ namespace SmartHome
     public partial class MainWindow : Window
     {
         public MainViewModel model { get; set; }
+        public ImageBrush myImage { get; set; }
+        //public Image myImage2 { get; set; }
 
         public MainWindow()
         {
             this.model = new MainViewModel();
             this.DataContext = model;
             InitializeComponent();
+
+            try
+            {
+                myImage = new ImageBrush(new BitmapImage(new Uri(@"../../images/people_icon.png", UriKind.Relative)));
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+            }
+            
+            /*
+            myImage = new Image();
+            BitmapImage myImageSource = new BitmapImage();
+            myImageSource.BeginInit();
+            myImageSource.UriSource = new Uri("images/people_icon.png");
+            myImageSource.EndInit();
+            myImage.Source = myImageSource;*/
+
+
             this.initLocationButtons();
         }
 
         public void initLocationButtons()
         {
+            /*
             int row = 0, column = 0;
             int maxRows = model.netatmoData.locationList.Count / 3;
             int remainingButtons = model.netatmoData.locationList.Count % 3;
@@ -45,22 +68,59 @@ namespace SmartHome
 
             foreach (Lieu lieu in model.netatmoData.locationList)
             {
-                Button locationButton = new Button();
-                locationButton.Content = lieu.name;
+                Grid locationButton = createLocationGrid(lieu);
                 Grid.SetColumn(locationButton, column);
                 Grid.SetRow(locationButton, row);
-                //locationButton.Click += clickOnLocationButton;
-                
+
                 if (row == maxRows - 1 && remainingButtons == 1)
                 {
                     Grid.SetColumnSpan(locationButton, 3);
                 }
                 BottomGrid.Children.Add(locationButton);
-                row = (column == 2) ? row+=1 : row;
-                column = (column == 2) ? column = 0 : column+=1;
+                row = (column == 2) ? row += 1 : row;
+                column = (column == 2) ? column = 0 : column += 1;
             }
         }
 
+        private Grid createLocationGrid(Lieu lieu)
+        {
+            Grid locationButton = new Grid();
+            RowDefinition header = new RowDefinition();
+            header.Height = new GridLength(40, GridUnitType.Star);
+            RowDefinition body = new RowDefinition();
+            body.Height = new GridLength(20, GridUnitType.Star);
+            RowDefinition footer = new RowDefinition();
+            footer.Height = new GridLength(40, GridUnitType.Star);
+
+            locationButton.RowDefinitions.Add(header);
+            locationButton.RowDefinitions.Add(body);
+            locationButton.RowDefinitions.Add(footer);
+
+            Grid bodyGrid = new Grid();
+            for (int i = 0; i < lieu.capteurList.Count; i++)
+            {
+                bodyGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            Label labelLieu = new Label { Content = lieu.name };
+            Grid.SetRow(labelLieu, 0);
+            locationButton.Children.Add(labelLieu);
+            Grid.SetRow(bodyGrid, 1);
+            locationButton.Children.Add(bodyGrid);
+
+            Grid numberOfPeopleGrid = new Grid();
+            for (int j = 0; j<3; j++)
+            {
+                numberOfPeopleGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                Label peopleNumber = new Label { Background = myImage };
+                peopleNumber.Height = locationButton.Height;
+                peopleNumber.Width = myImage.ImageSource.Width;
+                Grid.SetColumn(peopleNumber, j);                
+            }
+            Grid.SetRow(numberOfPeopleGrid, 2);
+            locationButton.Children.Add(numberOfPeopleGrid);
+            return (locationButton);*/
+        }
 
         /* EVENTS */
 
